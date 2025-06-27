bcde78211311f2c2474a1c30b26b48de4d034f08
calvinzosa <calvinzosa@outlook.com>
2025-06-27 12:22:24 +0800

    refactor(hooks): removed heartbeat connection from useStepped

--- src/client/ui/hooks/useStepped.ts
+++ src/client/ui/hooks/useStepped.ts
@@ -4,27 +4,15 @@ type CallbackFunction = (dt: number, time: number) => void;
 
 const callbacks = new Set<CallbackFunction>();
 
-if (!RunService.IsStudio() || RunService.IsRunning()) {
-	RunService.Stepped.Connect((time, dt) => {
-		for (const callback of callbacks) {
-			try {
-				callback(dt, time);
-			} catch (err) {
-				warn('[client::hooks/useStepped] an error occured in the useStepped hook:', err);
-			}
+RunService.Stepped.Connect((time, dt) => {
+	for (const callback of callbacks) {
+		try {
+			callback(dt, time);
+		} catch (err) {
+			warn('[client::hooks/useStepped] an error occured in the useStepped hook:', err);
 		}
-	});
-} else {
-	RunService.Heartbeat.Connect((dt) => {
-		for (const callback of callbacks) {
-			try {
-				callback(dt, os.clock());
-			} catch (err) {
-				warn('[client::hooks/useStepped] an error occured in the useStepped hook:', err);
-			}
-		}
-	});
-}
+	}
+});
 
 export function useStepped(callback: CallbackFunction): () => void {
 	callbacks.add(callback);
