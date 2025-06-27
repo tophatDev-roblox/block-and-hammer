a89ed59b9189da72ba0a89c3afe7ca2205e30506
calvinzosa <calvinzosa@outlook.com>
2025-06-27 18:52:38 +0800

    feat(string): add trim functions

--- src/shared/string.ts
+++ src/shared/string.ts
@@ -6,4 +6,16 @@ export default class String {
 	static endsWith(this: void, endString: string, str: string): boolean {
 		return str.sub(-endString.size(), -1) === endString;
 	}
+	
+	static trimStart(this: void, str: string): string {
+		return str.gsub('^%s+', '')[0];
+	}
+	
+	static trimEnd(this: void, str: string): string {
+		return str.gsub('%s+$', '')[0];
+	}
+	
+	static trim(this: void, str: string): string {
+		return String.trimEnd(String.trimStart(str));
+	}
 }
