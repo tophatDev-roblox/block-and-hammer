ea13cacb2fb0ccd2f7e44b41baae37f8896e7bae
calvinzosa <calvinzosa@outlook.com>
2025-06-28 23:27:22 +0800

    feat!(ui): remove error boundary

--- src/client/ui/components/App.tsx
+++ src/client/ui/components/App.tsx
@@ -1,7 +1,5 @@
 import React from '@rbxts/react';
 
-import ErrorBoundary from './ErrorBoundary';
-import Fallback from './Fallback';
 import SpeedEffect from './SpeedEffect';
 import Version from './Version';
 import HUD from './HUD';
@@ -10,19 +8,13 @@ import SideMenu from './SideMenu';
 
 const App: React.FC = () => {
 	return (
-		<ErrorBoundary
-			fallback={(err) => (
-				<Fallback
-					err={err}
-				/>
-			)}
-		>
+		<>
 			<SpeedEffect />
 			<Version />
 			<HUD />
 			<Timer />
 			<SideMenu />
-		</ErrorBoundary>
+		</>
 	);
 };
 
