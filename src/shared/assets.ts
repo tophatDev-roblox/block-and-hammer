634400f544106987556f2008db25242b017eacc5
calvinzosa <calvinzosa@outlook.com>
2025-06-29 22:10:24 +0800

    feat(assets): update badge icon

--- src/shared/assets.ts
+++ src/shared/assets.ts
@@ -1,6 +1,6 @@
 export namespace Assets {
 	export enum Icons {
-		BadgesIcon = 'rbxassetid://116428450407805',
+		BadgesIcon = 'rbxassetid://117939573736699',
 		CustomizeIcon = 'rbxassetid://126178460891242',
 		InventoryIcon = 'rbxassetid://124956783435550',
 		SettingsIcon = 'rbxassetid://132520229981046',
