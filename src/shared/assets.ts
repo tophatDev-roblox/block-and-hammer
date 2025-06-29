79e6f9f8a9e317fc371d62fe0687895c0c009137
calvinzosa <calvinzosa@outlook.com>
2025-06-29 21:25:17 +0800

    feat(assets): add assets namespace for asset ids

---
+++ src/shared/assets.ts
@@ -0,0 +1,10 @@
+export namespace Assets {
+	export enum Icons {
+		BadgesIcon = 'rbxassetid://116428450407805',
+		CustomizeIcon = 'rbxassetid://126178460891242',
+		InventoryIcon = 'rbxassetid://124956783435550',
+		SettingsIcon = 'rbxassetid://132520229981046',
+		SpectateIcon = 'rbxassetid://102925807754269',
+		StartMenuIcon = 'rbxassetid://136761769714294',
+	}
+}
