686ef85d24d7e6015da639b82b9d28acc5666a57
calvinzosa <calvinzosa@outlook.com>
2025-06-30 23:15:38 +0800

    fix(leaveSound): all sounds stop playing when leaving

--- src/client/leaveSound.ts
+++ src/client/leaveSound.ts
@@ -3,6 +3,13 @@ import { SoundService } from '@rbxts/services';
 const sound = SoundService.WaitForChild('Leave') as Sound;
 
 game.OnClose = () => {
+	for (const sound of SoundService.GetChildren()) {
+		if (sound.IsA('Sound')) {
+			sound.Stop();
+		}
+	}
+	
 	SoundService.PlayLocalSound(sound);
+	
 	task.wait(sound.TimeLength);
 };
