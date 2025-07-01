002308c33e1787b10249354e706c692997d525a2
calvinzosa <calvinzosa@outlook.com>
2025-07-01 10:33:35 +0800

    refactor(useAtomBinding): replace Charm import with Atom directly

--- src/client/ui/hooks/useAtomBinding.ts
+++ src/client/ui/hooks/useAtomBinding.ts
@@ -1,7 +1,7 @@
 import { useBinding, useEffect } from '@rbxts/react';
-import Charm, { peek, subscribe } from '@rbxts/charm';
+import { peek, subscribe, Atom } from '@rbxts/charm';
 
-export function useAtomBinding<T>(atom: Charm.Atom<T>): React.Binding<T> {
+export function useAtomBinding<T>(atom: Atom<T>): React.Binding<T> {
 	const [binding, setBinding] = useBinding<T>(peek(atom));
 	
 	useEffect(() => {
